5298968a233657d1b66b80226120b0509349927b
Luis <luisdiazvenero@gmail.com>
2018-04-24 16:38:43 -0500

    funcionalidad shuffle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
+  initPlayer()
   getSongs()
 
 })
 
 var audio = document.getElementById('player')
 var music
+
+function initPlayer(){
+  $('#shuffle').click(function(){
+    $('#playlist').empty()
+    console.log(shuffle(music.songs))
+    genList(music)
+    playSong(0)
+  })
+}
 
 function getSongs(){
   $.getJSON("./js/main.json", function(mjson){
@@ -15,9 +25,18 @@ function getSongs(){
 
 function playSong(id){
   console.log("el id es: " + id)
-  $('#img-album').attr('src',music.songs[id].image)
-  $('#player').attr('src',music.songs[id].song)
-  audio.play()
+  var long = music.songs
+  if(id>=long.length){
+    console.log("se acabo")
+    audio.pause()
+  } else {
+    $('#img-album').attr('src',music.songs[id].image)
+    $('#player').attr('src',music.songs[id].song)
+    audio.play()
+    console.log("hay mas canciones")
+    scheduleSong(id)
+  }
+
 }
 
 function genList(music){
@@ -29,4 +48,29 @@ function genList(music){
     //console.log(selectedsong)
     playSong(selectedsong)
   })
+}
+
+
+function scheduleSong(id){
+  audio.onended = function(){
+    console.log("termino la cancion")
+    playSong(parseInt(id)+1)
+
+  }
+}
+
+function shuffle(array){
+
+  var position = array.length, temp, random
+
+  while(0 !== position){
+
+    random = Math.floor(Math.random() * position)
+    position -= 1
+
+    temp = array[position]
+    array[position] = array[random]
+    array[random] = temp
+  }
+    return array
 }
