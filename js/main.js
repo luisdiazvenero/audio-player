76ba7b45c1d6bdb52c995ce28b057fe2ee20bb5b
Luis <luisdiazvenero@gmail.com>
2018-04-24 12:47:01 -0500

    Creando lista de canciones

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ var music
 function getSongs(){
   $.getJSON("./js/main.json", function(mjson){
     music = mjson
-    console.log(music)
+    genList(music)
+  })
+}
+
+function genList(music){
+  $.each(music.songs,function(i,song){
+    $('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.nombre+'</li>')
+  })
+  $('#playlist li').click(function(){
+    var selectedsong = $(this).attr('id')
+    console.log(selectedsong)
   })
 }
