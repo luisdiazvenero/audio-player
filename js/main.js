6b1c6390c2a788a45f54c393108153f1c5c045e4
Luis <luisdiazvenero@gmail.com>
2018-04-24 12:56:48 -0500

    reproducir cancion

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 })
 
-var audio
+var audio = document.getElementById('player')
 var music
 
 function getSongs(){
@@ -13,12 +13,20 @@ function getSongs(){
   })
 }
 
+function playSong(id){
+  console.log("el id es: " + id)
+  $('#img-album').attr('src',music.songs[id].image)
+  $('#player').attr('src',music.songs[id].song)
+  audio.play()
+}
+
 function genList(music){
   $.each(music.songs,function(i,song){
     $('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.nombre+'</li>')
   })
   $('#playlist li').click(function(){
     var selectedsong = $(this).attr('id')
-    console.log(selectedsong)
+    //console.log(selectedsong)
+    playSong(selectedsong)
   })
 }
